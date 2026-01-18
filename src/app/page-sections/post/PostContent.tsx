aa479eca4cf5020ff034821504021a2c02cb1100
Ramazan Dogan <doganrmzn40@gmail.com>
2026-01-18 12:28:24 +0300

    postContent had started to build

--- src/app/page-sections/post/PostContent.tsx
+++ src/app/page-sections/post/PostContent.tsx
@@ -8,11 +8,8 @@ type PostContentProps = {
 export default function PostContent({ content, title }: PostContentProps) {
   return (
     <div className="mx-auto">
-      <article className="prose prose-lg dark:prose-invert max-w-none">
-        <div
-          dangerouslySetInnerHTML={{ __html: content }}
-          className="prose-headings:font-bold prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-p:leading-relaxed prose-a:text-primary prose-a:underline prose-img:rounded-lg"
-        />
+      <article className="post-content">
+        <div dangerouslySetInnerHTML={{ __html: content }} />
       </article>
     </div>
   );
